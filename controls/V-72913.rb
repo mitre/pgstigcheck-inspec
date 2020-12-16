f5b9e925e59d35b98362acd831e646af1f0e7e8a
ejaronne <34140975+ejaronne@users.noreply.github.com>
2020-12-16 10:27:25 -0500

    contain cat find if input path doesn't exist

--- controls/V-72913.rb
+++ controls/V-72913.rb
@@ -99,6 +99,10 @@ control "V-72913" do
 
   describe command("cat `find #{pg_audit_log_dir} -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d\" \"` | grep \"permission denied to set parameter\"") do
     its('stdout') { should match /^.*permission denied to set parameter .pgaudit.role..*$/ }
-  end
+  end if file(pg_audit_log_dir).exist?
 
+  describe "The #{pg_audit_log_dir} directory was not found. Check path for this postgres version/install to define the value for the 'pg_audit_log_dir' inspec input parameter." do
+    skip "The #{pg_audit_log_dir} directory was not found. Check path for this postgres version/install to define the value for the 'pg_audit_log_dir' inspec input parameter."
+  end if !file(pg_audit_log_dir).exist?
+  
 end
