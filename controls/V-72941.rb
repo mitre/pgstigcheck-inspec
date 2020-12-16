18f8afba5033536f3e709c71f262ceb5d6a28199
ejaronne <34140975+ejaronne@users.noreply.github.com>
2020-12-16 11:39:45 -0500

    Update V-72941.rb

--- controls/V-72941.rb
+++ controls/V-72941.rb
@@ -89,6 +89,10 @@ control "V-72941" do
 
  describe command("cat `find #{pg_audit_log_dir} -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d\" \"` | grep \"permission denied for relation\"") do
    its('stdout') { should match /^.*pg_authid.*$/ }
- end
+  end if file(pg_audit_log_dir).exist?
+
+  describe "The #{pg_audit_log_dir} directory was not found. Check path for this postgres version/install to define the value for the 'pg_audit_log_dir' inspec input parameter." do
+    skip "The #{pg_audit_log_dir} directory was not found. Check path for this postgres version/install to define the value for the 'pg_audit_log_dir' inspec input parameter."
+  end if !file(pg_audit_log_dir).exist?
 
 end
