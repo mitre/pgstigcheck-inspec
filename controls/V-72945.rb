fcfe429791b94566a6240fcaed6e6e16b035f92d
ejaronne <34140975+ejaronne@users.noreply.github.com>
2020-12-16 11:40:19 -0500

    Update V-72945.rb

--- controls/V-72945.rb
+++ controls/V-72945.rb
@@ -85,6 +85,10 @@ control "V-72945" do
 
  describe command("cat `find #{pg_audit_log_dir} -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d\" \"` | grep \"permission denied to drop role\"") do
    its('stdout') { should match /^.*permission denied to drop role.*$/ }
- end
+  end if file(pg_audit_log_dir).exist?
+
+  describe "The #{pg_audit_log_dir} directory was not found. Check path for this postgres version/install to define the value for the 'pg_audit_log_dir' inspec input parameter." do
+    skip "The #{pg_audit_log_dir} directory was not found. Check path for this postgres version/install to define the value for the 'pg_audit_log_dir' inspec input parameter."
+  end if !file(pg_audit_log_dir).exist?
 
 end
